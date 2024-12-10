99f41bd2426b8c7885455a5bca74e5391482b6c5
sbharg <bhargav.samineni@gmail.com>
2024-12-10 00:37:27 +0000

    starting imm experiments

--- IMM/imm_experiments.sh
+++ IMM/imm_experiments.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-./imm_discrete -dataset ego-facebook-gnn/ -k 1 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-gnn/ -k 2 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-gnn/ -k 4 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-gnn/ -k 8 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-gnn/ -k 16 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-gnn/ -k 32 -model IC -epsilon 0.1
+./imm_discrete -dataset ego-facebook-gnn/ -k 1 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-gnn/ -k 2 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-gnn/ -k 4 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-gnn/ -k 8 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-gnn/ -k 16 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-gnn/ -k 32 -model IC -epsilon 0.03
 
-./imm_discrete -dataset ego-facebook-slicer/ -k 1 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-slicer/ -k 2 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-slicer/ -k 4 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-slicer/ -k 8 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-slicer/ -k 16 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-slicer/ -k 32 -model IC -epsilon 0.1
+./imm_discrete -dataset ego-facebook-slicer/ -k 1 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 2 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 4 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 8 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 16 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 32 -model IC -epsilon 0.03
 
-./imm_discrete -dataset ego-facebook-actual/ -k 1 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-actual/ -k 2 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-actual/ -k 4 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-actual/ -k 8 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-actual/ -k 16 -model IC -epsilon 0.1
-./imm_discrete -dataset ego-facebook-actual/ -k 32 -model IC -epsilon 0.1
+./imm_discrete -dataset ego-facebook-actual/ -k 1 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 2 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 4 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 8 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 16 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 32 -model IC -epsilon 0.03
