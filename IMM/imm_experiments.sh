b3a136bfb99e065fdb1d74da0308362fb0c4c2ce
sbharg <bhargav.samineni@gmail.com>
2024-12-10 05:55:44 +0000

    clean up

--- IMM/imm_experiments.sh
+++ IMM/imm_experiments.sh
@@ -8,16 +8,16 @@ eps=0.03
 ./imm_discrete -dataset ego-facebook-gnn/ -k 16 -model IC -epsilon $eps
 ./imm_discrete -dataset ego-facebook-gnn/ -k 32 -model IC -epsilon $eps
 
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 1 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 2 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 4 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 8 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 16 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-slicer/ -k 32 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-slicer/ -k 1 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-slicer/ -k 2 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-slicer/ -k 4 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-slicer/ -k 8 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-slicer/ -k 16 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-slicer/ -k 32 -model IC -epsilon $eps
 
-# ./imm_discrete -dataset ego-facebook-actual/ -k 1 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-actual/ -k 2 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-actual/ -k 4 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-actual/ -k 8 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-actual/ -k 16 -model IC -epsilon 0.03
-# ./imm_discrete -dataset ego-facebook-actual/ -k 32 -model IC -epsilon 0.03
+./imm_discrete -dataset ego-facebook-actual/ -k 1 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-actual/ -k 2 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-actual/ -k 4 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-actual/ -k 8 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-actual/ -k 16 -model IC -epsilon $eps
+./imm_discrete -dataset ego-facebook-actual/ -k 32 -model IC -epsilon $eps
